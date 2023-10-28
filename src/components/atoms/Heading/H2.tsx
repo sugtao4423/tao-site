8efc0fb8b1cf7694ec53e01e7c2458ec1a19d1e1
sugtao4423 <sugtao4423@gmail.com>
2023-10-29 05:17:00 +0900

    Support `className` for h2

--- src/components/atoms/Heading/H2.tsx
+++ src/components/atoms/Heading/H2.tsx
@@ -1,7 +1,9 @@
-import { PropsWithChildren } from '@/@types/react'
+import classNames from 'classnames'
+
+import { PropsWithClassChildren } from '@/@types/react'
 
 import styles from './h2.module.scss'
 
-export const H2: React.FC<PropsWithChildren> = (props) => (
-  <h2 className={styles.h2}>{props.children}</h2>
+export const H2: React.FC<PropsWithClassChildren> = (props) => (
+  <h2 className={classNames(props.className, styles.h2)}>{props.children}</h2>
 )
