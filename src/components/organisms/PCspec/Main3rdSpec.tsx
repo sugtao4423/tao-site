5834cfcbce418796dcbf5da4bcdba85b7068f8f4
sugtao4423 <sugtao4423@gmail.com>
2024-01-31 04:13:39 +0900

    メイン機のGPUがRTX4070 Ti SUPERに

--- src/components/organisms/PCspec/Main3rdSpec.tsx
+++ src/components/organisms/PCspec/Main3rdSpec.tsx
@@ -13,7 +13,7 @@ const specs: TableData<2> = [
   [null, 'D: Samsung SSD 870 EVO 4TB (SATA)'],
   [null, 'E: Samsung SSD 960 EVO 250GB (M.2 Type2280)'],
   [null, 'F: Samsung SSD 860 EVO 1TB (SATA)'],
-  ['GPU', 'GeForce RTX 2080'],
+  ['GPU', 'GeForce RTX 4070 Ti SUPER'],
   ['電源', 'KRPW-GK750W/90+'],
   ['OS', 'Win11 Pro 64bit'],
   ['ケース', 'Define R5'],
@@ -31,12 +31,15 @@ export const Main3rdSpec: React.FC = () => (
 
     <ul>
       <li>
-        2023/04/30に雑多なファイルを保存しているEドライブのSATA
-        SSDをM.2へ換装しました。
+        2023/04/30に雑多なファイルを保存しているEドライブのSATA SSDをM.2へ換装
       </li>
       <li>
         同年05/28にドキュメント等を保存しているDドライブの3TB HDDを4TBのSATA
-        SSDに換装しました。
+        SSDに換装
+      </li>
+      <li>
+        2024/01/30に発売されたばかりのRTX4070 Ti
+        SUPERが気になってしまい購入。RTX2080から換装
       </li>
     </ul>
   </Accordion>
