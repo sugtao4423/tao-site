87628be1d5fd95e51a57ee5429e72d0721284d54
sugtao4423 <sugtao4423@gmail.com>
2025-01-08 04:33:37 +0900

    ストレージサーバーに22TBのHDDを導入
    2024/11/12

--- src/components/organisms/PCspec/server/ServerStorage1stSpec.tsx
+++ src/components/organisms/PCspec/server/ServerStorage1stSpec.tsx
@@ -8,10 +8,9 @@ const specs: TableData<2> = [
   ['CPU', 'AMD Turion II Neo N54L Dual-Core'],
   ['メモリ', 'DDR3 1333MHz 16GB (8GBx2)'],
   ['HDD', 'WD Black 500GB SATA'],
-  [null, 'WD Green 3TB SATA (2015年9月)'],
-  [null, 'WD Blue 4TB SATA (2017年11月)'],
   [null, 'WD Red Plus 10TB SATA (2020年9月)'],
-  ['OS', 'Debian 10'],
+  [null, 'WD Ultrastar HC570 22TB SATA (2024年11月)'],
+  ['OS', 'Debian 12'],
 ]
 
 export const ServerStorage1stSpec: React.FC = () => (
@@ -43,6 +42,7 @@ export const ServerStorage1stSpec: React.FC = () => (
       <li>
         2018/01/20にメイン機の組み替えをしたことでDDR3のメモリが余り、4GBから8GBx2に換装
       </li>
+      <li>2024/11/12に22TBのHDDを購入し、3+4+10TBから10+22TBの構成に</li>
     </ul>
 
     <p>鯖室で稼働しています</p>
