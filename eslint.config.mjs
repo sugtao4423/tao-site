9248526b5867ba9d81149f200c947afcfb02d871
sugtao4423 <sugtao4423@gmail.com>
2025-04-11 06:01:59 +0900

    Add ESLint base configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,30 @@
-import { FlatCompat } from '@eslint/eslintrc'
+// @ts-check
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
+import eslint from '@eslint/js'
+import pluginNext from '@next/eslint-plugin-next'
+import configPrettier from 'eslint-config-prettier'
+import pluginImport from 'eslint-plugin-import'
+import tseslint from 'typescript-eslint'
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-]
-
-export default eslintConfig
+export default tseslint.config(
+  pluginNext.flatConfig.coreWebVitals,
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  pluginImport.flatConfigs.recommended,
+  { name: 'import/typescript', ...pluginImport.flatConfigs.typescript },
+  { name: 'eslint-config-prettier', ...configPrettier },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: { project: import.meta.dirname },
+      },
+    },
+  }
+)
