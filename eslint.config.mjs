3bc1765e25b4256666b62801d02b74321a163920
sugtao4423 <sugtao4423@gmail.com>
2025-04-11 19:07:08 +0900

    Add ESLint React configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,16 @@ import eslint from '@eslint/js'
 import pluginNext from '@next/eslint-plugin-next'
 import configPrettier from 'eslint-config-prettier'
 import pluginImport from 'eslint-plugin-import'
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  { name: 'react/recommended', ...pluginReact.configs.flat.recommended },
+  { name: 'react/jsx-runtime', ...pluginReact.configs.flat['jsx-runtime'] },
+  pluginReactHooks.configs['recommended-latest'],
+  pluginJsxA11y.flatConfigs.recommended,
   pluginNext.flatConfig.coreWebVitals,
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
