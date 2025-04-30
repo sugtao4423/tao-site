4a38b47642fb75622f5527941e4ce180a299b2c8
sugtao4423 <sugtao4423@gmail.com>
2025-04-30 18:41:16 +0900

    Support table header

--- src/components/molecules/Table/Table.tsx
+++ src/components/molecules/Table/Table.tsx
@@ -7,25 +7,41 @@ import styles from './table.module.scss'
 
 type Props = {
   data: TableData<number>
+  enableHeader?: boolean
   noVerticalDivider?: boolean
 } & PropsWithClassName
 
-export const Table: React.FC<Props> = (props) => (
-  <div className={classNames(styles.wrapper, props.className)}>
-    <table
-      className={classNames(styles.table, {
-        [styles.noVerticalDivider]: props.noVerticalDivider,
-      })}
-    >
-      <tbody>
-        {props.data.map((d, i) => (
-          <tr key={i}>
-            {d.map((v, j) => (
-              <td key={j}>{v}</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-)
+export const Table: React.FC<Props> = (props) => {
+  const thead = props.enableHeader ? props.data[0] : []
+  const tbody = props.enableHeader ? props.data.slice(1) : props.data
+
+  return (
+    <div className={classNames(styles.wrapper, props.className)}>
+      <table
+        className={classNames(styles.table, {
+          [styles.noVerticalDivider]: props.noVerticalDivider,
+        })}
+      >
+        {thead.length > 0 && (
+          <thead>
+            <tr>
+              {thead.map((v, i) => (
+                <th key={i}>{v}</th>
+              ))}
+            </tr>
+          </thead>
+        )}
+
+        <tbody>
+          {tbody.map((d, i) => (
+            <tr key={i}>
+              {d.map((v, j) => (
+                <td key={j}>{v}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
