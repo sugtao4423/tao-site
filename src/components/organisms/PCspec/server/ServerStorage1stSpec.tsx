663e045217ff7cd3d8f194a5c613eb062d40c8c8
sugtao4423 <sugtao4423@gmail.com>
2025-05-01 00:45:16 +0900

    マイクロ鯖のストレージに詳細な型番を追加

--- src/components/organisms/PCspec/server/ServerStorage1stSpec.tsx
+++ src/components/organisms/PCspec/server/ServerStorage1stSpec.tsx
@@ -7,16 +7,16 @@ import styles from '../spec.module.scss'
 const specs: TableData<2> = [
   ['CPU', 'AMD Turion II Neo N54L Dual-Core'],
   ['メモリ', 'DDR3 1333MHz 16GB (8GBx2)'],
-  ['HDD', 'WD Black 500GB (SATA)'],
-  [null, 'WD Red Plus 10TB (SATA)'],
-  [null, 'WD Ultrastar HC570 22TB (SATA)'],
+  ['HDD', 'WD Black 500GB MB0500EBNCR (SATA)'],
+  [null, 'WD Red Plus 10TB WD101EFAX (SATA)'],
+  [null, 'WD Ultrastar HC570 22TB WUH722222ALE6L4 (SATA)'],
   ['OS', 'Debian 12'],
 ]
 
 const replaceLog: TableData<5> = [
   ['日付', '種別', '換装前', '換装後', '備考'],
-  ['2015/09', 'HDD', null, 'WD Green 3TB (SATA)', '新規追加'],
-  ['2017/11', 'HDD', null, 'WD Blue 4TB (SATA)', '新規追加'],
+  ['2015/09', 'HDD', null, 'WD Green 3TB WD30EZRX (SATA)', '新規追加'],
+  ['2017/11', 'HDD', null, 'WD Blue 4TB WD40EZRZ (SATA)', '新規追加'],
   [
     '2018/01/20',
     'メモリ',
@@ -24,12 +24,12 @@ const replaceLog: TableData<5> = [
     'DDR3 1333MHz 16GB (8GBx2)',
     'メイン機の組み替えで余ったメモリを流用',
   ],
-  ['2020/09/25', 'HDD', null, 'WD Red Plus 10TB (SATA)', '新規追加'],
+  ['2020/09/25', 'HDD', null, 'WD Red Plus 10TB WD101EFAX (SATA)', '新規追加'],
   [
     '2024/11/12',
     'HDD',
-    'WD Green 3TB, WD Blue 4TB',
-    'WD Ultrastar HC570 22TB (SATA)',
+    'WD30EZRX, WD40EZRZ',
+    'WD Ultrastar HC570 22TB WUH722222ALE6L4 (SATA)',
     null,
   ],
 ]
